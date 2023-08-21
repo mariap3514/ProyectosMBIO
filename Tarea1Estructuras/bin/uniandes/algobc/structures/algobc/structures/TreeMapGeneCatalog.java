b8af91fd9121382a4705143ffebe3ce85212edae
mariap3514 <142847216+mariap3514@users.noreply.github.com>
2023-08-21 17:55:31 -0500

    Add files via upload

---
+++ Tarea1Estructuras/bin/uniandes/algobc/structures/algobc/structures/TreeMapGeneCatalog.java
@@ -0,0 +1,39 @@
+package uniandes.algobc.structures;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapGeneCatalog implements GeneCatalog {
+	private TreeMap<String, Gene> treeGene = new TreeMap<>();
+
+	@Override
+	public void addGene(Gene gene) {
+		// TODO Auto-generated method stub
+		treeGene.put(gene.getId(), gene);
+	}
+
+	@Override
+	public Gene getGene(String geneId) {
+		// TODO Auto-generated method stub
+		return treeGene.get(geneId);
+	}
+
+	@Override
+	public ArrayList<Gene> getGenes(String ontologyId) {
+		// TODO Auto-generated method stub
+		ArrayList<Gene> listaGen = new ArrayList<Gene>();
+		for(Map.Entry<String, Gene> x:treeGene.entrySet()) {
+			Gene gen = x.getValue();
+			for(int i=0; i<gen.getOntologies().size();i++) {
+				if(gen.getOntologies().get(i).getId().equals(ontologyId)) {
+					listaGen.add(gen);
+					break;
+				}
+			}
+		}
+		return listaGen;
+		
+	}
+
+}
